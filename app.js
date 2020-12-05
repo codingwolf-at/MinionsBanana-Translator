953adfc5c80da67a0955bd54cd6bb714ecc8f322
codingwolf-at <atameshwari9@gmail.com>
2020-12-05 13:49:48 +0530

    added output textbox

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var btnTranslate = document.querySelector('#btn-translate');
 var txtInput = document.querySelector('#txt-input');
+var txtOutput = document.querySelector('#txt-output');
 
 function clickHandler() {
-    console.log("input", txtInput.value);
+    console.log(txtInput);
+    console.log(txtOutput);
 }
-
-
 
 btnTranslate.addEventListener("click", clickHandler);
